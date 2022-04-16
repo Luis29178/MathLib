f315f26226c454123b8769353821cab4cabe74a5
Luis Bazaldua <LEBazaldua@student.fullsail.edu>
2022-04-16 13:50:44 -0400

    Add project files.

---
+++ MathLib/MyMathLib.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MathLib
+{
+    public class MyMathlib
+    {
+        float x;
+        float y;
+
+        public MyMathlib()
+        {
+            x = 0.0f;
+            y = 0.0f;
+        }
+
+        public MyMathlib(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        // Smallest positive value greater than 0
+        public float Epsilon
+        {
+            get
+            {
+                return float.Epsilon;
+            }
+        }
+
+        // Not a number
+        public float NaN
+        {
+            get
+            {
+                return float.NaN;
+            }
+        }
+
+        public float Add()
+        {
+            return x + y;
+        }
+
+        public float Subtract()
+        {
+            return x - y;
+        }
+
+        // Behavior 1: return 12.0 times 3.0 == 36.0
+        // Behavior 2: if x and y are float.PositiveInfinty then throw NotFiniteNumberException
+        public float Multiply()
+        {
+            float result = x * y;
+
+            if (float.IsPositiveInfinity(result))
+            {
+                throw new NotFiniteNumberException();
+            }
+
+            return result;
+        }
+
+
+        // Behavior 1: return 10.0 divided by 2.0 == 5.0
+        // Behavior 2: if x == float.MinValue and y == float.MinValue throw NotFiniteNumberException 
+        // Behavior 3: if y == 0 throw DivideByZero exception
+        public float Divide()
+        {
+            if (x == float.MinValue && y == float.MinValue)
+            {
+                throw new NotFiniteNumberException();
+            }
+
+            if (y == 0.0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return x / y;
+        }
+    }
+}
---
+++ MyMathTest/MyMathlibTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib.Tests
+{
+    [TestClass()]
+    public class MyMathlibTests
+    {
+        [TestMethod()]
+        public void DefaultConstroctor()
+        {
+            MyMathlib lib = new MyMathlib();
+            if (lib.X == 0.0f && lib.Y == 0.0f)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+            
+        }
+        [TestMethod()]
+        public void CustomConstroctor()
+        {
+            MyMathlib lib = new MyMathlib(2.3f,4.6f);
+            if (lib.X == 2.3f && lib.Y == 4.6f)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+        
+        [TestMethod]
+        public void EpsilonTest()
+        {
+            var lib = new MyMathlib(2.3f,4.6f);
+            if (lib.Epsilon == Single.Epsilon)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+    }
+}
